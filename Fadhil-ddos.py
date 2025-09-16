250cf8528a0226fbd81a003b76e475c4c3055635
ahmadsan272-ship-it <ahmadsan272@gmail.com>
2025-09-16 07:07:11 +0700

    Update Fadhil-ddos.py

--- Fadhil-ddos.py
+++ Fadhil-ddos.py
@@ -52,7 +52,7 @@ while attemps < 100:
     username = input("\033[33mEnter your username: \033[0m")
     password = input("\033[32mEnter your password: \033[0m")
 
-    if username == '6453' and password == 'kondangan-dl':
+    if username == 'salman' and password == 'kondangan-dl':
         print('Selamat datang di zona SALMAN SENDIRI')
         break
     else:
